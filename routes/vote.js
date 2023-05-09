9136b2776700336f5b085c248d704656e026332d
RizwanCSEDU25 <rizwanhrizvi@gmail.com>
2023-05-09 12:11:39 +0600

    hy

--- routes/vote.js
+++ routes/vote.js
@@ -29,8 +29,8 @@ routerVote.post("/login", async(req,res, next) => {
 
           const temp = await Poll.findOne({ _id: req.body.electId});
           console.log(moment(temp.startTime).format('lll'))
-          console.log(moment().subtract(6, 'hours').format('lll a'))
-          console.log(moment(temp.startTime).format('lll') - moment().subtract(6, 'hours').format('lll'))
+          console.log(moment().format('lll'))
+          console.log(moment.duration(moment(temp.startTime).format('lll').diff(moment().format('lll'))))
 
           if((temp.startTime - moment().subtract(6, 'hours').format('lll'))<=0){
             if((temp.endTime - moment().subtract(6, 'hours').format('lll') > 0)){
